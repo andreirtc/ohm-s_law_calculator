52e68baaa79280740da3372a994f1d4e10268043
Ralph Andrei Castillo <ralphandreicastillo326@gmail.com>
2024-09-25 12:28:39 +0800

    To have a more organized code, use OOP. First create a class and initialize instance variables needed for the program.

--- ohms_law_calculator.py
+++ ohms_law_calculator.py
@@ -1,3 +1,10 @@
+# 0. To have a more organized code, use OOP. First create a class and initialize instance variables needed for the program.
+class OhmsLawCalculator:
+    def __init__(self):
+        self.choice = None
+        self.current = None
+        self.voltage = None
+        self.resistance = None
 # 1. Ask the user what they want to calculate: Voltage, Current, or Resistance
 # 2. Based on their choice, prompt the user to input the appropriate values. 
 # 3. Calculate their input using Ohm's Law and display the result, while alsong considering cases where division by zero might occcur.
