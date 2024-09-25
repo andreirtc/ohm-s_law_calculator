2a483540b8e5b99b3189cc9ef15ea78462727c4e
Ralph Andrei Castillo <ralphandreicastillo326@gmail.com>
2024-09-25 12:12:18 +0800

    Pseudocode

---
+++ ohms_law_calculator.py
@@ -0,0 +1,4 @@
+# 1. Ask the user what they want to calculate: Voltage, Current, or Resistance
+# 2. Based on their choice, prompt the user to input the appropriate values. 
+# 3. Calculate their input using Ohm's Law and display the result, while alsong considering cases where division by zero might occcur.
+
